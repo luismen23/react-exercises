6190f9356fd532094b71d25c5cc6ac4d4ea49224
Luis Mendoza <lmmg1701@gmail.com>
2023-10-27 19:02:44 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// esta es la config de vite que necesito para cuando quiero crear un proyecto react con vanilla JS
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()]
-})
