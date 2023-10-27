86f2dd159ee4df8c737cccf862ce90a9d22b3c09
Luis Mendoza <lmmg1701@gmail.com>
2023-10-27 19:02:27 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-// esta es la config que necesito para recuperar el punto de entrada de mi app en el index.html para cuando quiero crear un proyecto react con vanilla JS
-
-import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
-
-const root = createRoot(document.getElementById('app'))
-root.render(<App />)
